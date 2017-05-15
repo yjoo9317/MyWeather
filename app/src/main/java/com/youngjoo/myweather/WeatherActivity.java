07dda2f87d6d079eaa6b7b7dcbc99671e25149d3
Jason Ryu and Shawn Kang <pair+jaeyeon.ryu+sh0708.kang@samsung.com>
2017-05-15 16:45:06 +0900

    add air korea uri and fragment activity

--- app/src/main/java/com/youngjoo/myweather/WeatherActivity.java
+++ app/src/main/java/com/youngjoo/myweather/WeatherActivity.java
@@ -1,15 +1,17 @@
 package com.youngjoo.myweather;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WeatherActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_weather_info);
     }
 
 }
--- app/src/main/java/com/youngjoo/myweather/WeatherFetcher.java
+++ app/src/main/java/com/youngjoo/myweather/WeatherFetcher.java
@@ -16,8 +16,10 @@ import java.net.URL;
 public class WeatherFetcher {
 
     private static final String APP_KEY="46d979b1-7eeb-3eee-ac3a-af6d1c98e8c3";
+    private static final String SERVICE_KEY="QtxQoigSv7WX2WSBh7AIeNGZF3HrGc74pFYOHHa1dlEn%2BdKEMTWHvJzfr3uI3K4ii0qmJ0GgLOfYB7BeSzm6jg%3D%3D";
     private static final String AIR_QUALITY_URI="http://apis.skplanetx.com/weather/airquality/current";
     private static final String WEATHER_URI="http://apis.skplanetx.com/weather/current/minutely";
+    private static final String AIR_QUALITY_BY_STATION_URI=" http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty";
     private static final String TAG="WeatherFetcher";
     private static final String LAT="37.274292";
     private static final String LON="127.076833";
@@ -34,6 +36,21 @@ public class WeatherFetcher {
             Log.i(TAG, "Received JSON: "+jsonStr);
         } catch (IOException ioe){
             Log.e(TAG, "Failed to fetch: "+ioe);
+        }
+    }
+
+    public void fetchFromAirKorea(){
+        try{
+            String url = Uri.parse(AIR_QUALITY_BY_STATION_URI).buildUpon()
+                    .appendQueryParameter("ServiceKey", SERVICE_KEY)
+                    .appendQueryParameter("ver", "1.3")
+                    .appendQueryParameter("stationName", "광교동")
+                    .appendQueryParameter("_returnType", "json")
+                    .build().toString();
+            String jsonResult = getResString(url);
+            Log.i(TAG, "Received JSON from air korea: "+jsonResult);
+        } catch(IOException ioe){
+            Log.e(TAG, "Failed to fetch from air korea: "+ioe);
         }
     }
 
