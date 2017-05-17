7d34b00c5a046ae13d851f31702767946c4e7418
Jason Ryu and Shawn Kang <pair+jaeyeon.ryu+sh0708.kang@samsung.com>
2017-05-17 16:57:56 +0900

    Location feature has just begun..

--- app/src/main/java/com/youngjoo/myweather/WeatherActivity.java
+++ app/src/main/java/com/youngjoo/myweather/WeatherActivity.java
@@ -1,16 +1,76 @@
 package com.youngjoo.myweather;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 
-public class WeatherActivity extends FragmentActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+public class WeatherActivity extends FragmentActivity
+        implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+    private static final String TAG="WeatherActivity";
+
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result){
+        Log.e(TAG, "Failed to connect Google Service: "+result.getErrorMessage());
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+                Log.e(TAG, "Permission denied..");
+        } else {
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if(mLastLocation != null){
+                Log.i(TAG, "<<<<<<<<<  Last Location >>>>>>>>>");
+                Log.i(TAG, "LAT: "+mLastLocation.getLatitude());
+                Log.i(TAG, "LONG: "+mLastLocation.getLongitude());
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i){
+
+    }
+
+    @Override
+    protected void onStart(){
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop(){
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
